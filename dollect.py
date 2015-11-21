29ec5578d14a6c8e7a139e86b78e5917ad2c2129
Jeffrin Jose T <jeffrin@rocketmail.com>
2015-11-21 21:55:10 +0530

    work

--- dollect.py
+++ dollect.py
@@ -11,25 +11,30 @@ def lva():
     string3 = "http://www.linuxvoice.com/issues/00"
     string4 = "/Linux-Voice-Issue-00"
     string7 = "Linux-Voice-Issue-00"
-    print string1 + string7 + str(i) + string2
     c = string3  + str(i)  + string4 
     d =  str(i) + string2
-    try:       
-        filename = wget.download(c+d)
-        print "\nCompleted\n"
+    try:
+        if i >= 1:
+            print string1 + string7 + str(i) + string2
+            filename = wget.download(c+d)
+            print "\nCompleted\n"
     except:
-        print "volume is from 1 to 12"
+        print "\n Did something else happen ?  \n"
     
 def lvb():
     string5 = "/Linux-Voice-Issue-0"
     string6 = "http://www.linuxvoice.com/issues/0"
     string8 = "Linux-Voice-Issue-0"
-    print string1 + string8  + str(i) + string2
     a = string6 + str(i)  + string5 
     b = str(i) + string2
-    filename = wget.download(a+b)
-    print "\nCompleted\n"
-
+    try:
+        if i <= 12:
+            print string1 + string8  + str(i) + string2
+            filename = wget.download(a+b)
+            print "\nCompleted\n"
+    except:
+        print "\n Did something else happen ?  \n"
+        
 if len(sys.argv) > 1:
     i = int(sys.argv[1])
 else:
