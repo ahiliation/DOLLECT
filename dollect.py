a33ad08bc7809ef745632311146fb05faac94aee
Jeffrin Jose T <jeffrin@rocketmail.com>
2015-11-22 21:25:57 +0530

    work

--- dollect.py
+++ dollect.py
@@ -7,6 +7,23 @@ global string2
 string1 = "Retrieving...  "
 string2 = ".pdf"
 
+off = list()
+off.append('how-to-make-mistakes-in-python.pdf')
+off.append('object-oriented-vs-functional-programming.pdf')
+off.append('analyzing-visualizing-data-f-sharp.pdf')
+off.append('java-the-legend.pdf')
+off.append('why-rust.pdf')
+off.append('introducing-java-8.pdf')
+off.append('engineering-managers-guide-design-patterns.pdf')
+off.append('azure-for-developers.pdf')
+off.append('c++-today.pdf')
+off.append('functional-programming-python.pdf')
+off.append('python-in-education.pdf')
+off.append('from-future-import-python.pdf')
+off.append('software-architecture-patterns.pdf')
+off.append('migrating-cloud-native-application-architectures.pdf')
+off.append('getting-started-with-innersource.pdf')
+
 def lva():
     string3 = "http://www.linuxvoice.com/issues/00"
     string4 = "/Linux-Voice-Issue-00"
@@ -34,10 +51,18 @@ def lvb():
             print "\nCompleted\n"
     except:
         print "\n Did something else happen ?  \n"
-        
-if len(sys.argv) > 1:
-    i = int(sys.argv[1])
-else:
+
+
+def oreilly():
+    string9  = "https://www.oreilly.com/"
+    string10 = "programming/free/files/"
+    e = string9 + string10
+    for count in xrange(len(off)):
+        f = off[count]
+        print string1 + f 
+        filename = wget.download(e+f)
+        print ("\n Completed \n")
+if sys.argv[1] == "lv":
     print "\n All LinuxVoice Download \n"
     for i in xrange(12):
         if i == 0:
@@ -46,10 +71,12 @@ else:
             lva()
         else:
             lvb()
-if i <= 9:
-#debug
-    lva()
-else:
-#debug
-    lvb()
-
+        if sys.argv[2] <= 9:
+            lva()
+        else:
+            #debug
+            lvb()
+            
+elif sys.argv[1] == "oreilly":
+    oreilly()
+    
