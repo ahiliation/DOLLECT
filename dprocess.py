7ddd28f561c8abf15d43d05f0940b80cc390a81c
Jeffrin Jose T <jeffrin@rocketmail.com>
2016-05-21 23:13:56 +0530

    work

--- dprocess.py
+++ dprocess.py
@@ -1,2 +1,11 @@
-import pdfminer
+from pdfrw import PdfReader, PdfWriter
+ 
+pages = PdfReader(r'book_008.pdf', decompress=False).pages
+other_pages = PdfReader(r'WebSocket.pdf', decompress=False).pages
 
+writer = PdfWriter()
+writer.addpages(pages)
+writer.addpages(other_pages)
+
+print PdfReader('book_008.pdf').values
+writer.write(r'out.pdf')
