60c74890f35350c4ecdac9e77313e8c8196697d1
Jeffrin Jose T <jeffrin@rocketmail.com>
2016-05-26 20:16:31 +0530

    work

--- testing/oop-port-testing.py
+++ testing/oop-port-testing.py
@@ -1,1 +1,10 @@
- 
+import wget
+
+class Dollect:
+    global html
+    html = "http://www.gnu.org/philosophy/philosophy.html"
+    def download(self):
+        filename = wget.download(html)
+
+dobject = Dollect()
+dobject.download()
