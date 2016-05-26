4c1238c1c053d79e71f7038e2327773b67282b44
Jeffrin Jose T <jeffrin@rocketmail.com>
2016-05-26 22:38:24 +0530

    work

--- testing/oop-port-testing.py
+++ testing/oop-port-testing.py
@@ -2,8 +2,13 @@ import wget
 
 class Dollect:
     global html
-    html = "http://www.gnu.org/philosophy/philosophy.html"
+    global arca
+    arca = list()
+    html = "http://audio-video.gnu.org/video/rms-education-es-high-sub.en.ogv"
+    arca = ['Cpio','Gzip','Paxutils','Sharutils','Tar','Xorriso']
     def download(self):
+        for package in arca:
+            print package
         filename = wget.download(html)
 
 dobject = Dollect()
