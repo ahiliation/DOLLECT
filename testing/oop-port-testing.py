637762f4d329b4ec6e964eab4320508449ee2e3c
Jeffrin Jose T <jeffrin@rocketmail.com>
2016-05-29 00:42:29 +0530

    work

--- testing/oop-port-testing.py
+++ testing/oop-port-testing.py
@@ -1,20 +1,72 @@
 import wget
+
 global ds
+global base
+base = "initial"
+global program
+program = "initial"
+
+dictionaries = list()
 ds = list()
+database = list()
+form = list()
+dlink = list()
 
-class Dollect:
-    def __init__(self, ds):
+class GNUDollect(base, program):
+    form.append('html')
+    form.append('info')
+    form.append('ascii')
+    form.append('dvi')
+    form.append('pdf')
+    form.append('texi')
+    html = base + program + "/manual/" + program + ".html"
+    info = base + program + "/manual/" + program + "-info.tar.gz"
+    text = base + program + "/manual/" + program + ".txt"
+    dvi  = base + program + "/manual/" + program + ".dvi.gz"
+    pdf  = base + program + "/manual/" + program + ".pdf"
+    texi = base + program + "/manual/" + program + ".texi.tar.gz"
+    dlink.append(html)
+    dlink.append(info)
+    dlink.append(text)
+    dlink.append(dvi)
+    dlink.append(pdf)
+    dlink.append(texi)
+                        
+   # def __init__(self, ds):
+   #     self.ds = ds
+   # global html
+
+    def pownload(self,ds):
         self.ds = ds
-      #  print ds
-    global html
-    for package in ds:
-        print package
-    html = "http://audio-video.gnu.org/video/rms-education-es-high-sub.en.ogv"
-    def download(self,ds):
-        for package in ds:
-            print package
-       # filename = wget.download(html)
+        print "[html/info/ascii/dvi/pdf/texi/all]"
+        selection = raw_input()
+        for name in xrange(6):
+            if selection == "all":
+                for name in xrange(6):
+                    filename = wget.download(dlink[name])
+                print "\n"
+                sys.exit(0)
+            else:
+                if form[name] == selection:
+                    try:
+                        filename = wget.download(dlink[name])
+                        print "\n"
+                    except:
+                        print "Unable to find URL \n"
 
-arca = ['Cpio','Gzip','Paxutils','Sharutils','Tar','Xorriso']
-dobject = Dollect(arca)
-dobject.download(arca)
+
+
+dictionaries.append('Dico')
+print "[Dico]"
+selection = raw_input()
+selection = selection.lower()
+dict = GNUDollect("http://www.gnu.org/software/", selection)
+dict.pownload(dictionaries)		            
+			        
+  
+
+database.append('Gdbm')
+database.append('Libdbh')
+
+
+
