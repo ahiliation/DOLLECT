181baa1042d918cec7bcf8407a793fe37dab7f82
Jeffrin Jose T <ahiliation@yahoo.co.in>
2016-05-29 01:52:46 +0530

    work

--- testing/oop-port-testing.py
+++ testing/oop-port-testing.py
@@ -1,4 +1,5 @@
 import wget
+import sys
 
 global ds
 global base
@@ -12,25 +13,26 @@ database = list()
 form = list()
 dlink = list()
 
-class GNUDollect(base, program):
-    form.append('html')
-    form.append('info')
-    form.append('ascii')
-    form.append('dvi')
-    form.append('pdf')
-    form.append('texi')
-    html = base + program + "/manual/" + program + ".html"
-    info = base + program + "/manual/" + program + "-info.tar.gz"
-    text = base + program + "/manual/" + program + ".txt"
-    dvi  = base + program + "/manual/" + program + ".dvi.gz"
-    pdf  = base + program + "/manual/" + program + ".pdf"
-    texi = base + program + "/manual/" + program + ".texi.tar.gz"
-    dlink.append(html)
-    dlink.append(info)
-    dlink.append(text)
-    dlink.append(dvi)
-    dlink.append(pdf)
-    dlink.append(texi)
+class GNUDollect():
+    def __init__(self, base, program):
+        form.append('html')
+        form.append('info')
+        form.append('ascii')
+        form.append('dvi')
+        form.append('pdf')
+        form.append('texi')
+        html = base + program + "/manual/" + program + ".html"
+        info = base + program + "/manual/" + program + "-info.tar.gz"
+        text = base + program + "/manual/" + program + ".txt"
+        dvi  = base + program + "/manual/" + program + ".dvi.gz"
+        pdf  = base + program + "/manual/" + program + ".pdf"
+        texi = base + program + "/manual/" + program + ".texi.tar.gz"
+        dlink.append(html)
+        dlink.append(info)
+        dlink.append(text)
+        dlink.append(dvi)
+        dlink.append(pdf)
+        dlink.append(texi)
                         
    # def __init__(self, ds):
    #     self.ds = ds
@@ -40,19 +42,19 @@ class GNUDollect(base, program):
         self.ds = ds
         print "[html/info/ascii/dvi/pdf/texi/all]"
         selection = raw_input()
-        for name in xrange(6):
-            if selection == "all":
-                for name in xrange(6):
+#        for name in xrange(6):
+        if selection == "all":
+            for name in xrange(6):
+                filename = wget.download(dlink[name])
+            print "\n"
+            sys.exit(0)
+        else:
+            if form[name] == selection:
+                try:
                     filename = wget.download(dlink[name])
-                print "\n"
-                sys.exit(0)
-            else:
-                if form[name] == selection:
-                    try:
-                        filename = wget.download(dlink[name])
-                        print "\n"
-                    except:
-                        print "Unable to find URL \n"
+                    print "\n"
+                except:
+                    print "Unable to find URL \n"
 
 
 
@@ -60,7 +62,7 @@ dictionaries.append('Dico')
 print "[Dico]"
 selection = raw_input()
 selection = selection.lower()
-dict = GNUDollect("http://www.gnu.org/software/", selection)
+dict = GNUDollect("http://puszcza.gnu.org.ua/software/", selection)
 dict.pownload(dictionaries)		            
 			        
   
