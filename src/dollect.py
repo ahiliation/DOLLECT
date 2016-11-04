fd5719d50a904d8015d21b1076e60a4173922e23
Jeffrin Jose T <ahiliation@yahoo.co.in>
2016-11-04 21:19:51 +0530

    update related

--- src/dollect.py
+++ src/dollect.py
@@ -1045,7 +1045,7 @@ def lvb():
     a = stringf + str(i)  + stringe 
     b = str(i) + stringb
     try:
-        if i <= 20:
+        if i <= 23:
 #           print stringa + stringh  + str(i) + stringb
             convertednext = decimal_roman(i)
             print stringa + " " +"LINUXVOICE" + " " + convertednext
@@ -1089,7 +1089,7 @@ if len(sys.argv) > 1:
             connectivity()
             lva()
             sys.exit(0)
-        elif int(sys.argv[2]) <= 20:
+        elif int(sys.argv[2]) <= 23:
             #debug
             i = int(sys.argv[2])
             connectivity()
@@ -1179,7 +1179,7 @@ if len(sys.argv) > 1:
         
     else:
         print "\n LINUXVOICE COMPLETE \n"
-        for i in xrange(20):
+        for i in xrange(23):
             if i == 0:
                 continue
             if i <= 9:
