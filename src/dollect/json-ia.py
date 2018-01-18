a8d82ac25256427913ec0a7f61b529050e8db096
Jeffrin Jose <ahiliation@yahoo.co.in>
2018-01-18 21:39:32 +0530

    work related

--- src/dollect/json-ia.py
+++ src/dollect/json-ia.py
@@ -1,15 +1,36 @@
 import urllib
 import json
 import sys
+import wget
 
-url = "http://archive.org/metadata/NationalGeographic1955"
-input = urllib.urlopen(url).read()
+# url = "http://archive.org/metadata/NationalGeographic1955"
+
+#x = -1
+
+name = raw_input("item name? ")
+
+
+#print "\nTesting JSON related\n"
+
+#print type(info)
+
+#print info['files'][0]['name']
+
+urld = "http://archive.org/metadata/" + name
+
+input = urllib.urlopen(urld).read()
 
 info = json.loads(input)
 
-print "\nTesting JSON related\n"
-
-print type(info)
-
-print info['files'][0]['name']
- 
+# print type(info)
+for key, value in info.iteritems():
+   # x = x + 1
+    print key
+   # print "\n"
+   # print "hello"
+    if key == "files":
+        for x in range(len(info[key])):
+            print x  
+            if  info[key][x]['format'] == "Text PDF":
+                url = "http://archive.org/download/" + name + "/" + info[key][x]['name']
+                filename = wget.download(url)
