2ed677f3858278536e2b11f284fbbfda000545ab
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2018-06-15 00:01:45 +0530

    Update item.py

--- src/dollect/item.py
+++ src/dollect/item.py
@@ -1,4 +1,4 @@
-Copyright 2018 Jeffrin Jose T
+Copyright 2018 Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
