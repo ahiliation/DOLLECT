02d585231230c155e5ad9e8bbc566622e6f9d3ec
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2018-06-15 18:15:37 +0530

    Update linuxforyou.py

--- src/dollect/linuxforyou.py
+++ src/dollect/linuxforyou.py
@@ -51,7 +51,7 @@ def download_file(url):
     f.close()
     return
 
-
+/* This function is related to https://gist.github.com/dehowell/884204 */
 def file_avail(url):
     request = urllib2.Request(url)
     request.get_method = lambda : 'HEAD'
