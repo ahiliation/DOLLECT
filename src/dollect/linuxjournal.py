bd0c4d6739ebdc5db79fd729d2b04d52cf9e5528
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2018-06-15 18:17:13 +0530

    Update linuxjournal.py

--- src/dollect/linuxjournal.py
+++ src/dollect/linuxjournal.py
@@ -38,7 +38,8 @@ def download_file(url):
     print "\n"
     print " Linux Journal -- Year :", year , "Month:", month
     filename = wget.download(url) 
-
+	
+/* This function is related to https://gist.github.com/dehowell/884204 */
 def file_avail(url):
     request = urllib2.Request(url)
     request.get_method = lambda : 'HEAD'
