ce7b5f67c8b420aa35a6e91d521827e6aaa0e0f2
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2020-05-23 23:08:00 +0530

    update related

--- src/dollect/acm.py
+++ src/dollect/acm.py
@@ -1,14 +1,36 @@
 import requests
-import re
+import h11
+#import re
+import wget
+import urllib3
+import sys
+import os
 
-dpage = open("3.html", "rt") 
+
+year = 1958
+vol  = 1
+no = 1
+
+link = "https://dl.acm.org/toc/cacm/"+ str(year) +  "/" + str(vol) + "/" + str(no)
+print (link)
+initial = requests.get(link, allow_redirects=True)
+#print(initial)
+open('hello.txt', 'wb').write(initial.content)
+
+dpage = open("hello.txt" , "rt") 
 contents = dpage.read()      # read the entire file into a string
 dpage.close()   
 strfile = contents.find('doi')
+print (type(strfile))
+#newstr = strfile[4:10]
+#print (newstr)
 #print (strfile)
 #print (type(contents))
-print (contents[491:511])
-
-#url = 'https://dl.acm.org/doi/pdf/10.1145/3396208'
-#myfile = requests.get(url, allow_redirects=True)
-#open('hello.pdf', 'wb').write(myfile.content)
+print (contents[343:362])
+newstr = contents[343:362]
+newcontents = newstr[5:]
+print (newcontents)
+newlink = "https://dl.acm.org/doi/pdf/" + newcontents + "." + "368689"
+print (newlink)
+solfile = requests.get(newlink, allow_redirects=True)
+open('hello', 'wb').write(solfile.content)
