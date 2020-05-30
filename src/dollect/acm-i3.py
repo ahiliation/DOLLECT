f408f42a9ef3ac55d121158504453f4ba16cfbdd
Jeffrin Jose T <jeffrin@rajagiritech.edu.in>
2020-05-31 00:21:52 +0530

    update related

--- src/dollect/acm-i3.py
+++ src/dollect/acm-i3.py
@@ -7,6 +7,9 @@ import sys
 import os
 from bs4 import BeautifulSoup 
 import lxml
+import time
+from time import sleep
+from tqdm import tqdm
 
 year = 1958
 vol  = 1
@@ -57,15 +60,38 @@ print (type(documentname))
 
 linknumber = 368718
 newlink = "https://dl.acm.org/doi/pdf/" + newcontents + "." + str(linknumber)
-#print (newlink)
+#newlink = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.15.tar.xz"
+r = requests.get(newlink, allow_redirects=True, stream=True)
+print (dir(r.url.title))
+print (r.url)
+# print (type(r))
+# Total size in bytes.                                                                                                 t
+# total_size = int(r.headers.get('content-length', 0))
+total_size = int(r.headers.get('content-length', 0))
+#total_size = int(r.headers.get('content-length', 0))/(32*1024))
+#total_size = 208896
+print (total_size)
+block_size = 1024 #1 Kibibyte                                                                                          
+t=tqdm(total=total_size, unit='B', unit_scale=True)
+with open(documentname[1].a.contents[0] + "." + "pdf", 'wb') as f:
+    for data in r.iter_content(block_size):
+       # sleep(0.1)
+        t.update(len(data))
+        f.write(data)
+t.close()
+if total_size != 0 and t.n != total_size:
+    print("ERROR, something went wrong")
+              
+    #print (newlink)
 #print ("hello")
-solfile = requests.get(newlink, allow_redirects=True)
-open(documentname[1].a.contents[0] + "." + "pdf", 'wb').write(solfile.content)
+#solfile = requests.get(newlink, allow_redirects=True)
+#open(documentname[1].a.contents[0] + "." + "pdf", 'wb').write(solfile.content)
 
 for i in [3]:
     linknumber = i + linknumber
     newlink = "https://dl.acm.org/doi/pdf/" + newcontents + "." + str(linknumber)
   #  print (newlink)
     solfile = requests.get(newlink, allow_redirects=True)
+  #  print (type(solfile))
     open(documentname[3].a.contents[0] + "." + "pdf", 'wb').write(solfile.content)
 
